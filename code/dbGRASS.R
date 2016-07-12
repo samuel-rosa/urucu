13563995f2c8837c5fd9b0c727a3b35510d61901
samuel-rosa <alessandrosamuelrosa@gmail.com>
2016-07-12 17:06:02 -0300

    prepared grass gis database

--- code/dbGRASS.R
+++ code/dbGRASS.R
@@ -5,21 +5,17 @@ rm(list = ls())
 
 # CREATE GRASS GIS DATABASE ###################################################################################
 
-# Load elevation raster surface.
-elevation <- rgdal::readGDAL("data/grid/mdehc5x5.tif")
-str(elevation)
-
 # Set GRASS GIS DATABASE
 dbGRASS <- "data/GRASS"
 spgrass6::initGRASS(
-  gisBase = "/usr/lib/grass64/", gisDbase = dbGRASS, SG = elevation, location = "urucu", mapset = "database",
+  gisBase = "/usr/lib/grass64/", gisDbase = dbGRASS, location = "urucu", mapset = "database",
   override = TRUE, pid = Sys.getpid())
 
 # Set PERMANENT coordinate reference system
 spgrass6::initGRASS(
   gisBase = "/usr/lib/grass64/", gisDbase = dbGRASS, location = "urucu", mapset = "PERMANENT",
   override = TRUE, pid = Sys.getpid())
-system("g.proj -c epsg=32720")
+system("g.proj -c epsg=29190")
 
 # Update projection information in 'database'
 spgrass6::initGRASS(
@@ -27,15 +23,14 @@ spgrass6::initGRASS(
   override = TRUE, pid = Sys.getpid())
 system("g.region -d")
 
-# Load elevation raster surface and set it as default
-spgrass6::writeRAST(elevation, "elevation")
-system("g.region rast=elevation")
-spgrass6::gmeta()
-rm(elevation)
-gc()
-
 # LOAD RASTER SURFACES INTO DATABASE ##########################################################################
 dir <- "/home/lgcs-mds/projects/urucu/data/grid/"
+
+# Elevation
+system(paste("r.in.gdal input=", dir, "mdehc5x5.tif output=elevation", sep = ""))
+system("r.info map=elevation")
+system("g.region rast=elevation")
+spgrass6::gmeta()
 
 # Aspect
 system(paste("r.in.gdal input=", dir, "aspect.tif output=aspect", sep = ""))
@@ -85,82 +80,38 @@ system("r.info map=twi")
 system(paste("r.in.gdal input=", dir, "northerness.tif output=northerness", sep = ""))
 system("r.info map=northerness")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Load boundary vector to GRASS and use it to create a MASK.
-boundary <- raster::shapefile("data/QGIS/boundary.shp")
-spgrass6::writeVECT(boundary, "boundary")
-system("v.to.rast -h")
-system("v.to.rast input=boundary output=boundary use=val")
-system("r.mask input=boundary")
+# Check raster surfaces
 system("d.mon start=x0")
-system("d.rast past_landuse")
+system("d.rast twi")
 system("d.mon stop=x0")
 
-# CURRENT LAND USE ############################################################################################
-# Load current land use data to GRASS
-current_landuse <- raster::shapefile("data/QGIS/landuse.shp")
-current_landuse@data$landuse <- as.factor(current_landuse@data$landuse)
-current_landuse@data$id <- as.integer(c(1, 1, 0))
-spgrass6::writeVECT(current_landuse, "current_landuse", v.in.ogr_flags = "overwrite")
-system("v.to.rast -h")
-system("v.to.rast --o input=current_landuse output=current_landuse column=id")
+rm(dir)
 
-system("d.mon x0")
-system("d.rast current_landuse")
-system("d.mon stop=x0")
+# LOAD VECTOR DATA INTO DATABASE ##############################################################################
 
-# FIGURES #####################################################################################################
+dir <- "/home/lgcs-mds/projects/urucu/data/vector/"
 
-# Current land use
-load(file = "data/R/pointData.rda")
-pointData <- pointData[seq(1, nrow(pointData), 5), c("x", "y")]
-map <- 
-  sp::spplot(
-    current_landuse, "landuse", col.regions = c("lemonchiffon", "darkolivegreen1"), col = c("lightgray"),
-    scales = list(draw = TRUE), 
-    panel = function(x, y, ...) {
-      sp::panel.polygonsplot(x, y, ...)
-      lattice::panel.points(
-        x = pointData$x, y = pointData$y, pch = 21, fill = "darkolivegreen3", col.symbol = "dimgrey")})
-dev.off()
-png("res/fig/current-landuse.png", width = 700, height = 700, res = 150)
-map
-dev.off()
-rm(map)
+# Target soil map
+system(paste("v.in.ogr dsn=", dir, "MCS_Simplif_Poligon_correct.shp output=target_soil_map", sep = ""))
+system("v.info map=target_soil_map")
 
-# Past land use
-past_landuse <- spgrass6::readRAST("past_landuse")
-past_landuse$past_landuse <- (past_landuse$past_landuse - min(past_landuse$past_landuse, na.rm = TRUE)) / 
-  (max(past_landuse$past_landuse, na.rm = TRUE) - min(past_landuse$past_landuse, na.rm = TRUE))
-past_landuse$past_landuse <- round(past_landuse$past_landuse, 4)
-past_landuse <- as(past_landuse, "SpatialPixelsDataFrame")
-map <- 
-  sp::spplot(
-    past_landuse, "past_landuse", col.regions = rev(terrain.colors(100)), scales = list(draw = TRUE),
-    at = seq(0, 1, 0.01),
-    panel = function(x, y, ...) {
-      lattice::panel.levelplot(x, y, ...)
-      lattice::panel.points(
-        x = pointData$x, y = pointData$y, pch = 21, fill = "darkolivegreen3", col.symbol = "dimgrey")})
+# Outer limit
+system(paste("v.in.ogr dsn=", dir, "Lim50000Semidet.shp output=outer_limit", sep = ""))
+system("v.info map=outer_limit")
 
-dev.off()
-png("res/fig/past-landuse.png", width = 700, height = 700, res = 150)
-map
-dev.off()
-rm(map)
+# Accesible area
+system(paste("v.in.ogr dsn=", dir, "Lim10000Detal.shp output=access_limit", sep = ""))
+system("v.info map=access_limit")
+
+# Non-accessible area
+# system(paste("v.in.ogr dsn=", dir, "Lim25000Semidet.shp output=non_access_limit", sep = ""))
+# system("v.info map=non_access_limit")
+
+# # Load boundary vector to GRASS and use it to create a MASK.
+# boundary <- raster::shapefile("data/QGIS/boundary.shp")
+# spgrass6::writeVECT(boundary, "boundary")
+# system("v.to.rast -h")
+# system("v.to.rast input=boundary output=boundary use=val")
+# system("r.mask input=boundary")
+
+rm(dir)

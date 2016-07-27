eb5fd9468a66ede31bde8cc3c4d157e048c2a9c0
samuel-rosa <alessandrosamuelrosa@gmail.com>
2016-07-27 15:46:01 -0300

    completed processing of spatial data

--- code/dbGRASS.R
+++ code/dbGRASS.R
@@ -122,21 +122,11 @@ system("d.mon x0")
 
 # Elevation
 system(paste("r.in.gdal input=", dir, "elevation.tif output=elevation", sep = ""))
-system("r.info map=elevation")
+system("r.info elevation")
 system("g.region rast=elevation")
 spgrass6::gmeta()
 system("d.rast.leg elevation")
 system("d.histogram elevation")
-system("r.mask elevation")
-
-# Aspect
-# ArcGIS help: Cells in the input raster that are flat—with zero slope—are assigned an aspect of -1.
-# Given the conditions of the study area, it seems reasonable to set -1 as 0º. But let us first decide if 
-# aspect will be used as a covariate or will be dropped from the dataset.
-system(paste("r.in.gdal input=", dir, "aspect.tif output=aspect", sep = ""))
-system("r.info map=aspect")
-system("d.rast.leg aspect")
-system("d.histogram aspect")
 
 # Upslope flow path length
 system(paste("r.in.gdal input=", dir, "flow_up.tif output=flow_up", sep = ""))
@@ -145,53 +135,60 @@ system("d.rast.leg flow_up")
 system("d.histogram flow_up")
 # This covariate has a very assymetric distribution. It would be wise to reduce the assymetry to yielding
 # stronger correlations with the target soil variable. One solution is to use a logarithmic transform (but we
-# could also create categories).
+# could also create categories). Round values to the second decimal place.
 cmd <- c("r.mapcalc 'ln_flow_up=log(flow_up + 1)'")
+system(cmd)
+cmd <- paste("r.mapcalc 'ln_flow_up = double(round(ln_flow_up * 100)) * 0.01'")
 system(cmd)
 system("r.info ln_flow_up")
 system("d.rast.leg ln_flow_up")
 system("d.histogram ln_flow_up")
 
 # Curvature
-# This covariate has a very peaky distribution, with most values around zero. Besides, it may be necessary to
-# round values to the second decimal place. A solution might be to create categories.
+# This covariate has a very peaky distribution, with most values around zero. A solution might be to create
+# categories.
+# Round values to the second decimal place.
 system(paste("r.in.gdal input=", dir, "curvature.tif output=curvature", sep = ""))
+cmd <- paste("r.mapcalc 'curvature = double(round(curvature * 100)) * 0.01'")
+system(cmd)
 system("r.info curvature")
 system("d.rast.leg curvature")
 system("d.histogram curvature")
 
 # Profile curvature
-# This covariate has a very peaky distribution, with most values around zero. Besides, it may be necessary to
-# round values to the second decimal place. A solution might be to create categories.
+# This covariate has a very peaky distribution, with most values around zero. A solution might be to create
+# categories.
+# Round values to the second decimal place.
 system(paste("r.in.gdal input=", dir, "prof_curv.tif output=prof_curv", sep = ""))
+cmd <- paste("r.mapcalc 'prof_curv = double(round(prof_curv * 100)) * 0.01'")
+system(cmd)
 system("r.info prof_curv")
 system("d.rast.leg prof_curv")
 system("d.histogram prof_curv")
 
 # Plan curvature
-# This covariate has a very peaky distribution, with most values around zero. Besides, it may be necessary to
-# round values to the second decimal place. A solution might be to create categories.
+# This covariate has a very peaky distribution, with most values around zero. A solution might be to create
+# categories.
+# Round values to the second decimal place.
 system(paste("r.in.gdal input=", dir, "plan_curv.tif output=plan_curv", sep = ""))
+cmd <- paste("r.mapcalc 'plan_curv = double(round(plan_curv * 100)) * 0.01'")
+system(cmd)
 system("r.info plan_curv")
 system("d.rast.leg plan_curv")
 system("d.histogram plan_curv")
 
 # Slope
-# It may be necessary to round values to the second decimal place.
 # The empirical distribution is very assymetric, with most values close to zero. Larger values are found close
 # to the drainage network.
+# Correct flat surfaces adding 0.1º to the data. Then round values to the second decimal place.
 system(paste("r.in.gdal input=", dir, "slope.tif output=slope", sep = ""))
+cmd <- paste("r.mapcalc 'slope =slope + 0.1'")
+system(cmd)
+cmd <- paste("r.mapcalc 'slope = double(round(slope * 100)) * 0.01'")
+system(cmd)
 system("r.info slope")
 system("d.rast.leg slope")
 system("d.histogram slope")
-
-# Flow direction
-# This covariate has erroneous values outside the study area. This will likely not be a problem because we use 
-# a masking variable.
-system(paste("r.in.gdal input=", dir, "flow_dir.tif output=flow_dir", sep = ""))
-system("r.info flow_dir")
-system("d.rast.leg flow_dir")
-system("d.histogram flow_dir")
 
 # Downslope flow path length
 system(paste("r.in.gdal input=", dir, "flow_down.tif output=flow_down", sep = ""))
@@ -202,36 +199,33 @@ system("d.histogram flow_down")
 # Flow accumulation
 # Like flow direction, this covariate has erroneous values outside the study area. This will likely not be a
 # problem because we use a masking variable.
+# Compute catchment area using the cell size (25 m²)
 system(paste("r.in.gdal input=", dir, "flow_accum.tif output=flow_accum", sep = ""))
+cmd <- paste("r.mapcalc 'flow_accum = (flow_accum + 1) * 25'")
+system(cmd)
 system("r.info flow_accum")
 system("d.rast.leg flow_accum")
 system("d.histogram flow_accum")
-# This covariate has an assymetric distribution. It would could be helpful to transform it using a logarithmic
-# function.
-cmd <- paste("r.mapcalc 'ln_flow_accum = log(flow_accum + 1)'")
+# This covariate has an assymetric distribution. It could be helpful to transform it using a logarithmic
+# function. Then round values to the second decimal place.
+cmd <- paste("r.mapcalc 'ln_flow_accum = log(flow_accum)'")
+system(cmd)
+cmd <- paste("r.mapcalc 'ln_flow_accum = double(round(ln_flow_accum * 100)) * 0.01'")
 system(cmd)
 system("r.info ln_flow_accum")
 system("d.rast.leg ln_flow_accum")
 system("d.histogram ln_flow_accum")
 
-
-
-
-
-
-
 # Topographic wetness index
-system(paste("r.in.gdal input=", dir, "itu5x5.tif output=twi", sep = ""))
-system("r.info map=twi")
-
-# Northerness
-system(paste("r.in.gdal input=", dir, "northerness.tif output=northerness", sep = ""))
-system("r.info map=northerness")
-
-# Check raster surfaces
-system("d.mon start=x0")
-system("d.rast twi")
-
+# Reference: Wilson and Gallant (2000)
+# Round values to the second decimal place.
+cmd <- paste("r.mapcalc 'twi = log((flow_accum / 5) / tan(slope))'", sep = "")
+system(cmd)
+cmd <- paste("r.mapcalc 'twi = double(round(twi * 100)) * 0.01'")
+system(cmd)
+system("r.info twi")
+system("d.rast.leg twi")
+system("d.histogram twi")
 
 system("d.mon stop=x0")
 rm(dir)
@@ -241,27 +235,24 @@ rm(dir)
 dir <- "/home/lgcs-mds/projects/urucu/data/vector/"
 
 # Target soil map
-system(paste("v.in.ogr dsn=", dir, "MCS_Simplif_Poligon_correct.shp output=target_soil_map", sep = ""))
+
+system(paste("v.in.ogr dsn=", dir, "target_soil_map.shp output=target_soil_map", sep = ""))
 system("v.info map=target_soil_map")
 
 # Outer limit
-system(paste("v.in.ogr dsn=", dir, "Lim50000Semidet.shp output=outer_limit", sep = ""))
+system(paste("v.in.ogr dsn=", dir, "outer_area.shp output=outer_limit", sep = ""))
 system("v.info map=outer_limit")
 
 # Accesible area
-system(paste("v.in.ogr dsn=", dir, "Lim10000Detal.shp output=access_limit", sep = ""))
+system(paste("v.in.ogr dsn=", dir, "access_area.shp output=access_limit", sep = ""))
 system("v.info map=access_limit")
 
 # Non-accessible area
-# system(paste("v.in.ogr dsn=", dir, "Lim25000Semidet.shp output=non_access_limit", sep = ""))
-# system("v.info map=non_access_limit")
+system(paste("v.in.ogr dsn=", dir, "non_access_area.shp output=non_access_limit", sep = ""))
+system("v.info map=non_access_limit")
 
-# # Load boundary vector to GRASS and use it to create a MASK.
-# boundary <- raster::shapefile("data/QGIS/boundary.shp")
-# spgrass6::writeVECT(boundary, "boundary")
-# system("v.to.rast -h")
-# system("v.to.rast input=boundary output=boundary use=val")
-# system("r.mask input=boundary")
+# Transform vector to raster to create a MASK.
+system("v.to.rast input=non_access_limit output=non_access_limit use=val")
+system("r.mask --o input=non_access_limit")
 
 rm(dir)
-

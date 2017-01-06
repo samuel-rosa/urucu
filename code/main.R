98d4600d0406e2886bb23861a4cfb62ea20b2337
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-01-05 22:43:58 -0200

    Replaced code chunk used to produce figure on the order of tacit sampling. Also cleaned up the code and improved the figure.

--- code/main.R
+++ code/main.R
@@ -150,26 +150,6 @@ cal_expert <- raster::shapefile("data/vector/Trein_Classes.shp")
 out <- sp::over(cal_expert, non_access_limit)
 out <- which(is.na(sp::over(cal_expert, non_access_limit)$cat))
 cal_expert <- cal_expert[-out, ]
-
-# Check the order in which map units were "sampled" by the expert
-tmp <- cal_expert$MDS
-tmp <- ifelse(tmp == 3, 2, tmp)
-tmp <- ifelse(tmp == 4, 3, tmp)
-tmp <- ifelse(tmp == 5, 4, tmp)
-x <- data.frame(y = rep(1, length(tmp)), x = 1:length(tmp))
-p <- lattice::xyplot(
-  y ~ x, data = x, col = soil.colors[tmp], pch = 15, cex = 0.25, type = "h", ylim = c(0, 1),
-  key = list(text = list(labels = um_levels), rect = list(col = soil.colors), columns = 2),
-  ylab = "", xlab = "Sampling order", scales = list(y = list(draw = FALSE)), xlim = c(1, length(tmp)))
-p$x.scales$at <- seq(1, length(tmp), length.out = 5)
-p$par.settings <- list(fontsize = list(text = 12))
-dev.off()
-png("res/fig/expert_sampling_order.png", width = 480 * 2, height = 480 * 0.5, res = 72 * 2)
-p
-dev.off()
-rm(tmp, x, p)
-
-# Continue with processing
 cal_expert$UM <- as.factor(cal_expert$MDS)
 levels(cal_expert$UM) <- levels(cal_field$UM)[c(1, 2, 2, 3, 4)]
 cal_expert@data <- data.frame(UM = cal_expert$UM)
@@ -311,7 +291,7 @@ xy <- rbind(xy, ma)
 p <- lattice::barchart(
   values ~ um | ind, data = xy, col = soil.colors,
   scales = list(x = list(draw = FALSE)), ylab = "Proportion",
-  key = list(text = list(as.character(unique(xy$um))), rectangles = list(col = soil.colors)),
+  key = list(text = list(as.character(unique(xy$um))), rect = list(col = soil.colors), columns = 2),
   par.settings = list(fontsize = list(text = 8, points = 6)),
   panel = function (...) {
     lattice::panel.grid(v = 0, h = -1)
@@ -345,6 +325,20 @@ png("res/fig/balanced_sampling.png", width = 480*4, height = 480*2, res = 250)
 p
 dev.off()
 rm(p, tmp, id, n)
+
+# Figure. Order in which map units were "sampled" by the expert.
+x <- data.frame(y = 1, x = 1:length(cal_expert$UM))
+p <- lattice::xyplot(
+  y ~ x, data = x, col = soil.colors[cal_expert$UM], pch = 15, cex = 0.25, type = "h", ylim = c(0, 1),
+  key = list(text = list(labels = levels(cal_expert$UM)), rect = list(col = soil.colors), columns = 2),
+  ylab = "", xlab = "Sampling order", scales = list(y = list(draw = FALSE)), 
+  xlim = c(1, length(cal_expert$UM)))
+p$x.scales$at <- seq(1, length(cal_expert$UM), length.out = 5)
+dev.off()
+png("res/fig/expert_sampling_order.png", width = 480 * 4, height = 480 * 1, res = 250)
+p
+dev.off()
+rm(x, p)
 
 # Calibrate soil prediction models ############################################################################
 # For Fisher's linear discriminant we compute the fitted category at each calibration point so that later
